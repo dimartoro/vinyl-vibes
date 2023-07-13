09b6ff39755b0e7027c80c1146cc6d6e4866ffd5
taylor-green <taylor.green2804@gmail.com>
2023-07-13 16:12:42 -0400

    working on profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import {useQuery, useMutation } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 import { Link } from 'react-router-dom';
 import { DELETE_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
@@ -17,18 +17,18 @@ function Profile() {
 
   const deleteHandler = async (e) => {
     e.preventDefault();
-    if(window.confirm("Are you sure you want to delete your account?") == true){
+    if (window.confirm("Are you sure you want to delete your account?") == true) {
       var id = data.user._id;
       const mutationResponse = await deleteUser({
         variables: {
           id
         },
-      }).then((result)=>{
-        if(result.data.deleteUser){
+      }).then((result) => {
+        if (result.data.deleteUser) {
           alert("Your account has been deleted. Please come back to do business!")
           Auth.logout();
         }
-      }).catch((error)=>{
+      }).catch((error) => {
         alert("There was an error deleting your profile");
       });
     }
@@ -37,39 +37,48 @@ function Profile() {
   return (
     <>
       <div className="container my-1">
+
+
+
         {user ? (
-          <>
-            <h2>
-              User Profile
-            </h2>
-            <div>First Name: {user.firstName}</div>
-            <div>Last Name: {user.lastName}</div>
-            <div>Email: {user.email}</div>
-            <br/>
-            {user.orders.map((order) => (
-              <div key={order._id} className="my-2">
-                <h3>
-                  {new Date(parseInt(order.purchaseDate)).toLocaleDateString()}
-                </h3>
-                <div className="flex-row">
-                  {order.products.map(({ _id, image, name, price }, index) => (
-                    <div key={index} className="card px-1 py-1">
-                      <Link to={`/products/${_id}`}>
-                        <img alt={name} src={`/images/${image}`} />
-                        <p>{name}</p>
-                      </Link>
-                      <div>
-                        <span>${price}</span>
-                      </div>
-                    </div>
-                  ))}
+            <>
+            <div style={{ backgroundImage: "url('https://wallpaper.dog/large/20450684.jpg')", justifyContent: "center", alignItems: "center", backgroundSize: "cover", height:"100vh", width:"100vw" }}>
+
+              <h2>
+                User Profile
+              </h2>
+              <div>First Name: {user.firstName}</div>
+              <div>Last Name: {user.lastName}</div>
+              <div>Email: {user.email}</div>
+           
+              <br />
+              {user.orders.map((order) => (
+          <div key={order._id} className="my-2">
+            <h3>
+              {new Date(parseInt(order.purchaseDate)).toLocaleDateString()}
+            </h3>
+            <div className="flex-row">
+              {order.products.map(({ _id, image, name, price }, index) => (
+                <div key={index} className="card px-1 py-1">
+                  <Link to={`/products/${_id}`}>
+                    <img alt={name} src={`/images/${image}`} />
+                    <p>{name}</p>
+                  </Link>
+                  <div>
+                    <span>${price}</span>
+                  </div>
                 </div>
-              </div>
-            ))}
-            <div> I want to <button onClick={deleteHandler} id={`${user._id}`}> Delete </button> my account</div>
-          </>
-        ) : null}
-      </div>
+              ))}
+            </div>
+          </div>
+        ))}
+        <div> I want to <button onClick={deleteHandler} id={`${user._id}`}> Delete </button> my account</div>
+        </div>
+      </>
+
+
+          ) : null}
+    </div>
     </>
   );
 }
